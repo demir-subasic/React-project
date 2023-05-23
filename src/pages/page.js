26a49fdd69825f1a5d4f41b708bc218d6e9f4aa7
demirsubasic2001 <subasicd2000@gmail.com>
2023-05-23 20:15:35 +0200

    navlink-added

--- src/pages/page.js
+++ src/pages/page.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import "./page.css"
-import {Link, Route, Routes } from 'react-router-dom';
+import {NavLink, Route, Routes } from 'react-router-dom';
 
 export default function Page() {
   return (
     <div className='page'>
       <nav>
         <ul className='page-ul'>
-        <li style={{listStyle: 'none'}}><Link style={{textDecoration: 'none', color: 'white'}} to="/home">Home</Link></li>
-        <li style={{listStyle: 'none'}}><Link style={{textDecoration: 'none', color: 'white'}} to="/about">About</Link></li>
-        <li style={{listStyle: 'none'}}><Link style={{textDecoration: 'none', color: 'white'}} to="/info">Info</Link></li>
-        <li style={{listStyle: 'none'}}><Link style={{textDecoration: 'none', color: 'white'}} to="/more">More</Link></li>
+        <li style={{listStyle: 'none'}}><NavLink className={({isActive}) => (isActive ? 'active' : '')} style={{textDecoration: 'none', color: 'white'}} to="/home">Home</NavLink></li>
+        <li style={{listStyle: 'none'}}><NavLink style={{textDecoration: 'none', color: 'white'}} to="/about">About</NavLink></li>
+        <li style={{listStyle: 'none'}}><NavLink style={{textDecoration: 'none', color: 'white'}} to="/info">Info</NavLink></li>
+        <li style={{listStyle: 'none'}}><NavLink style={{textDecoration: 'none', color: 'white'}} to="/more">More</NavLink></li>
         </ul>
       </nav>
     </div>
