99d939bc0577fff8a39f96bebd180946666d6fa0
demirsubasic2001 <subasicd2000@gmail.com>
2023-07-06 18:31:02 +0200

    react-quiz

--- src/quiz/quiz.js
+++ src/quiz/quiz.js
@@ -1,45 +1,72 @@
-import React, {useState} from 'react'
-import './quiz.styled'
-import { ButtonWrapper, Main, Title, SingleButton } from './quiz.styled';
+import React, { useState } from 'react';
+import {QuizContainer, Question, AnswerButton, ScoreSection, Result } from './quiz.styled'
+
+
 
 const Quiz = () => {
+  const questions = [
+    {
+      questionText: 'Koji je glavni grad Francuske?',
+      answerOptions: [
+        { answerText: 'Pariz', isCorrect: true },
+        { answerText: 'London', isCorrect: false },
+        { answerText: 'Berlin', isCorrect: false },
+        { answerText: 'Rim', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Koja je najduza reka na svijetu?',
+      answerOptions: [
+        { answerText: 'Nil', isCorrect: true },
+        { answerText: 'Amazon', isCorrect: false },
+        { answerText: 'Sena', isCorrect: false },
+        { answerText: 'Dunav', isCorrect: false },
+      ],
+    },
+  ];
 
-    const [result, setResult] = useState(0);
-    const [answer, setAnswer] = useState('');
-    const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
 
-    const questions = [
-        {
-          questionText: 'Koji je glavni grad Francuske?',
-          answerOptions: [
-            { answerText: 'London', isCorrect: false },
-            { answerText: 'Berlin', isCorrect: false },
-            { answerText: 'Pariz', isCorrect: true },
-            { answerText: 'Madrid', isCorrect: false },
-          ],
-        },
-        {
-          questionText: 'Koji je najveći kontinent na svijetu?',
-          answerOptions: [
-            { answerText: 'Afrika', isCorrect: false },
-            { answerText: 'Europa', isCorrect: false },
-            { answerText: 'Azija', isCorrect: true },
-            { answerText: 'Australija', isCorrect: false },
-          ],
-        },
-    ];
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+
   return (
-    <>
-    <Main>
-    <Title>QUIZ</Title>
-    <ButtonWrapper>
-        <SingleButton>
-            aa
-        </SingleButton>
-    </ButtonWrapper>
-    </Main>
-    </>
-  )
-}
+    <QuizContainer>
+      {showScore ? (
+        <ScoreSection>
+          <Result>
+            Pogodio si {score} od {questions.length} tacnih odgovora.
+          </Result>
+        </ScoreSection>
+      ) : (
+        <>
+          <Question>Pitanje {currentQuestion + 1}</Question>
+          <p>{questions[currentQuestion].questionText}</p>
+          {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+            <AnswerButton
+              key={index}
+              onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}
+            >
+              {answerOption.answerText}
+            </AnswerButton>
+          ))}
+        </>
+      )}
+    </QuizContainer>
+  );
+};
 
-export default Quiz
+export default Quiz;
--- src/quiz/quiz.styled.js
+++ src/quiz/quiz.styled.js
@@ -1,30 +1,49 @@
 import styled from 'styled-components';
 
-export const Main = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+export const QuizContainer = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`
+
+export const Question = styled.h3`
+  font-size: 18px;
+  margin-bottom: 10px;
+`
+
+export const AnswerButton = styled.button`
+  display: block;
   width: 100%;
-  height: 60vh;
-  background-color: yellow;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: none;
+  border-radius: 4px;
+  background-color: #fff;
+  color: #333;
+  font-size: 14px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
 `
 
-export const Title = styled.h1`
-  font-size: 30px;
-  color: aqua;
+export const ScoreSection = styled.div`
+  text-align: center;
 `
 
-export const ButtonWrapper = styled.div`
-   background-color: black;
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   width: 30%;
+export const Result = styled.p`
+  font-size: 16px;
+  margin-bottom: 20px;
 `
 
-export const SingleButton = styled.button`
-   background-color: aqua;
-   font-size: 18px;
-   width: 100px;
+export const RestartButton = styled.button`
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  background-color: #4caf50;
+  color: #fff;
+  font-size: 16px;
+  cursor: pointer;
 `
